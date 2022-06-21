0e70659d43fb7f8d0337c0802c53ea87bbd68856
zai747 <alimv121@gmail.com>
2022-06-22 01:23:42 +0530

    finished

--- walkingman.js
+++ walkingman.js
@@ -1,39 +1,67 @@
 
-$("#start").click(() => { go()})
-$("#return").click(() => { goback()} )
 var margin = 0;
+let stopflag = false
 
 
-    let go =() => {
-        $("#wimg").removeClass("start")
-        $("#wimg").addClass("walk")
-        var interval =  setInterval(() => {
+$("#start").click(() =>  go())
+$("#return").click(() => goback())
+$("#stop").click(() => stopflag = true)
+
+
+let go =() => {
+
+    stopflag = false
+    $("#wimg").removeClass("start")
+    $("#wimg").addClass("walk")
+
+        if ($("#wimg").hasClass("flip")) 
+        {
+            $("#wimg").toggleClass("flip")
+        }
+    
+            let interval =  setInterval(() => {
                 $("#wimg").css({"margin-left": margin + "%" })
                 console.log(margin);
                 margin += 1 ;
+                if (margin == 80) {
+                    $("#wimg").toggleClass("flip")
+                }
                 clear(margin,interval)
-              
-        }, 100);
-          }
             
+            }, 90);
+}
+    
+    
     const clear = (margin,interval) => {
-        if (!(margin > 0 && margin < 90))
+        
+        if ( (!(margin > 0 && margin < 80)) || stopflag == true)
         {
             clearInterval(interval)
             
-            $("#wimg").toggleClass("flip")
         }
-            }
-    const goback = () => {
-            interval =  setInterval(() => {
+       
+    }
 
-                
+
+    const goback = () => {
+
+          stopflag = false
+          let interval =  setInterval( () => {
+
+            if (!($("#wimg").hasClass("flip"))) 
+            {
+                $("#wimg").toggleClass("flip")
+            }
+        
                 $("#wimg").css({"margin-left": margin + "%" })
                 console.log(margin);
                 margin -= 1 ;
+                if (margin == 0) {
+                    $("#wimg").toggleClass("flip")
+                }
                 clear(margin,interval)
-              
-        }, 100);
+            }, 90);
+                     
     }
        
 
