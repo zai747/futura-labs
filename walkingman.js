8e8550406a660d43c659977c6fdab07c630c94ad
zai747 <alimv121@gmail.com>
2022-06-22 00:06:51 +0530

    working

--- walkingman.js
+++ walkingman.js
@@ -1,36 +1,41 @@
 
-        $("#start").click(() => {
+$("#start").click(() => { go()})
+$("#return").click(() => { goback()} )
+var margin = 0;
 
-                $("#wimg").removeClass("start")
-                $("#wimg").addClass("walk")
 
-                var margin = 0;
-
-               
-                
-              var interval =  setInterval(() => {
-
-                
-                        $("#wimg").css({"margin-left": margin + "%" })
-                        console.log(margin);
-                        margin += 1 ;
-                        clear(margin)
-                      
-                }, 100);
-        function clear(margin) {
-            if (margin == 90)
-            {
-                clearInterval(interval)
-                $("#wimg").addClass("flip")
-                
-            }
+    let go =() => {
+        $("#wimg").removeClass("start")
+        $("#wimg").addClass("walk")
+        var interval =  setInterval(() => {
+                $("#wimg").css({"margin-left": margin + "%" })
+                console.log(margin);
+                margin += 1 ;
+                clear(margin,interval)
+              
+        }, 100);
+          }
+            
+    const clear = (margin,interval) => {
+        if (!(margin > 0 && margin < 90))
+        {
+            clearInterval(interval)
+            
+            $("#wimg").toggleClass("flip")
         }
-                
-        })
-        $("#stop").click(() => {
+            }
+    const goback = () => {
+            interval =  setInterval(() => {
 
-            $("#wimg").stop()
-        } )
+                
+                $("#wimg").css({"margin-left": margin + "%" })
+                console.log(margin);
+                margin -= 1 ;
+                clear(margin,interval)
+              
+        }, 100);
+    }
+       
 
         
 
